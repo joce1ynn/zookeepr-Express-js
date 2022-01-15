fa6f494a0015aa0cadcaab9ead4fba29c2795715
Wenwen Tian <wwtian9@gmail.com>
2022-01-15 13:15:41 -0700

    add POST

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+//app.use() The functions are referred to as middleware.
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -42,6 +50,38 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    //在子目录中写入animals.json文件
+    path.join(__dirname, "./data/animals.json"),
+    //将 JavaScript数组数据保存为JSON,null意味着我们不想编辑任何现有数据
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  // return finished code to post route for response
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+// to transfer data from server to client (GET)
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -59,6 +99,29 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
+// If we make a GET request to /api/animals, then the app.get('/api/animals') callback function will execute.
+// But if it's a POST request, it'll go to the one we just created
+// to transfer data from client to server (POST)
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+// GET 路由很容易测试，因为我们可以简单地进入浏览器并在搜索栏中输入 URL
+// 对于 POST 请求，最常见的客户端方法是编写一个前端应用程序，该应用程序发出fetch()请求以将数据发送到服务器。
+// 但是我们还没有前端，仅仅为了测试而构建一个似乎是在浪费时间。
+// 我们可以使用一个名为 Insomnia 的应用程序来测试我们的 API
